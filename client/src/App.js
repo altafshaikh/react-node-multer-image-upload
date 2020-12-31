7172c6736086f2f2c8b4ee6158bf5549e261dc42
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-31 09:17:30 +0530

    multer rendered in app file

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Multer from "./components/Multer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Multer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
